df6f28ec2efe97478c3efe4c34ff17aff8d6990f
Gerard nunes <gerard321nuneslima123@gmail.com>
2026-02-26 14:39:46 -0300

    commit versin 1.0

--- gestao_hoffman/settings.py
+++ gestao_hoffman/settings.py
@@ -50,11 +50,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gestao_hoffman.wsgi.application'
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
